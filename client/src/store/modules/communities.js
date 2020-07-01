30771dd49cdc72e7949f944899f7b9f697aff10b
Tyler Bunting <tdbunting91@gmail.com>
2020-07-01 09:56:56 -0700

    added communities module to vue state

---
+++ client/src/store/modules/communities.js
@@ -0,0 +1,21 @@
+import feathers from '../../feathers';
+
+export default {
+  namespaced: true,
+  state: {
+    loading: false,
+    error: '',
+    messages: [],
+    communities: [],
+  },
+  actions: {
+    async getCommunities({ state }) {
+      try {
+        state.loading = true;
+        const communities = await feathers.service('communities').find({});
+        state.communities = communities.data;
+      } catch (e) {}
+      state.loading = false;
+    },
+  },
+};
