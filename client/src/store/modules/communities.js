e774901c4774bcc09ac2e4f993bae57d67e448f1
Tyler Bunting <tdbunting91@gmail.com>
2020-07-02 12:47:26 -0700

    moved _icons.scss

--- client/src/store/modules/communities.js
+++ client/src/store/modules/communities.js
@@ -1,21 +1,21 @@
-import feathers from '../../feathers';
+import feathers from '../../feathers'
 
 export default {
   namespaced: true,
   state: {
     loading: false,
     error: '',
-    messages: [],
+    currentCommunity: null,
     communities: [],
   },
   actions: {
     async getCommunities({ state }) {
       try {
-        state.loading = true;
-        const communities = await feathers.service('communities').find({});
-        state.communities = communities.data;
-      } catch (e) {}
-      state.loading = false;
+        state.loading = true
+        const communities = await feathers.service('communities').find({})
+        state.communities = communities.data
+      } catch (e) { }
+      state.loading = false
     },
   },
-};
+}
